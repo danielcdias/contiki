e08d132185433ef4cfe13d0304c85986aaf032c5
Daniel Carvalho Dias <daniel.dias@gmail.com>
2019-09-09 10:51:19 -0300

    Correções para servidor em ambiente docker.

--- ceiot/inc10/projeto-final/server/start-django.sh
+++ ceiot/inc10/projeto-final/server/start-django.sh
@@ -3,13 +3,13 @@
 # pip install -r requirements.txt
 echo "********** Waiting database get ready..."
 python wait_for_db.py db $MYSQL_USER $MYSQL_PASSWORD $MYSQL_DATABASE 12 5 "SELECT version()"
- echo "********** Running Django makemigrations command..."
- python manage.py makemigrations
- echo "********** Running Django migrate command..."
- python manage.py migrate
- echo "********** Running Django collectstatic command..."
- python manage.py collectstatic --no-input
- # python manage.py collectstatic -link --noinput
- chmod 777 /app/static -R
- echo "********** Running Django server..."
- python manage.py runserver 0.0.0.0:8000
+echo "********** Running Django makemigrations command..."
+python manage.py makemigrations
+echo "********** Running Django migrate command..."
+python manage.py migrate
+echo "********** Running Django collectstatic command..."
+python manage.py collectstatic --no-input
+# python manage.py collectstatic -link --noinput
+chmod 777 /app/static -R
+echo "********** Running Django server..."
+python manage.py runserver 0.0.0.0:8000
