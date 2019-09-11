4b9ce7b0ab1aa0bd6ccea6553daa25ecf0d72037
Daniel Carvalho Dias <daniel.dias@gmail.com>
2019-09-10 23:54:45 -0300

    Correções na configuração do docker.

--- ceiot/inc10/projeto-final/server/start-django.sh
+++ ceiot/inc10/projeto-final/server/start-django.sh
@@ -3,8 +3,6 @@
 # pip install -r requirements.txt
 echo "********** Waiting database get ready..."
 python wait_for_db.py db $MYSQL_USER $MYSQL_PASSWORD $MYSQL_DATABASE 12 5 "SELECT version()"
-echo "********** Running Django makemigrations command..."
-python manage.py makemigrations
 echo "********** Running Django migrate command..."
 python manage.py migrate
 echo "********** Running Django collectstatic command..."
